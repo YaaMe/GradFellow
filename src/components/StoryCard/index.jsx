9de0c6b8e4f1a1d98cf75d802deade5b08a50556
YaaMe <969519545@qq.com>
2018-10-11 20:17:22 +0900

    chore(StoryCard)

---
+++ src/components/StoryCard/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from 'antd';
+
+
+export default ({ name }) => (
+  <Card
+    title={name}
+  >
+    <p>Card content</p>
+    <p>Card content</p>
+    <p>Card content</p>
+  </Card>
+)
