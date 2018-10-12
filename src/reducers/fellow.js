ea8490a32862e6939217a2602a80edcad3834a7d
YaaMe <969519545@qq.com>
2018-10-12 18:47:26 +0900

    chore(StoriesCard): pagination

--- src/reducers/fellow.js
+++ src/reducers/fellow.js
@@ -67,6 +67,57 @@ const initState = {
     },
     companyCulture: {
     }
+  }, {
+    firstName: 'Cindy',
+    lastName: 'Xhang',
+    currentPosition: 'Business Analyst',
+    currentCompany: 'EY',
+    nationality: '',
+    contacted: false,
+    skills: {
+      startWith: ['3 months Python Subject', '12 weeks SQL in Uni', '5 years of Microsoft Office Usagev'],
+      learned: [],
+      recommanded: '',
+      roles: [],
+      tasks: [],
+      perform: []
+    },
+    companyCulture: {
+    }
+  }, {
+    firstName: 'Cindy',
+    lastName: 'Xhang',
+    currentPosition: 'Business Analyst',
+    currentCompany: 'EY',
+    nationality: '',
+    contacted: false,
+    skills: {
+      startWith: ['3 months Python Subject', '12 weeks SQL in Uni', '5 years of Microsoft Office Usagev'],
+      learned: [],
+      recommanded: '',
+      roles: [],
+      tasks: [],
+      perform: []
+    },
+    companyCulture: {
+    }
+  }, {
+    firstName: 'Cindy',
+    lastName: 'Xhang',
+    currentPosition: 'Business Analyst',
+    currentCompany: 'EY',
+    nationality: '',
+    contacted: false,
+    skills: {
+      startWith: ['3 months Python Subject', '12 weeks SQL in Uni', '5 years of Microsoft Office Usagev'],
+      learned: [],
+      recommanded: '',
+      roles: [],
+      tasks: [],
+      perform: []
+    },
+    companyCulture: {
+    }
   }]
 };
 
