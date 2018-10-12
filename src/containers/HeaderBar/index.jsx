9c45f1b2098e1c1809ee978c68a7244ba0cabeea
YaaMe <969519545@qq.com>
2018-10-13 02:19:41 +0900

    chore(proj): update login

--- src/containers/HeaderBar/index.jsx
+++ src/containers/HeaderBar/index.jsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import { Layout, Menu } from 'antd';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { logo } from 'css/imgs';
+import { Row, Col, Layout, Menu, Button, Popover } from 'antd';
 import { Link } from 'react-router-dom';
+import LoginForm from 'containers/LoginForm';
 
 const { Header } = Layout;
 
-const renderMenuItem = (menu, index) => (
-  <Menu.Item key={index}>
-    <Link to={`/${menu}`}>{menu}</Link>
-  </Menu.Item>
-)
+const mapStateToProps = ({ user }) => ({ user });
+const mapDispatchToProps = dispatch => ({});
 
-export default ({ items }) => (
-  <Header>
-    <div className="logo"/>
-    <Menu
-      theme="dark"
-      mode="horizontal"
-      defaultSelectedKeys={['2']}
-      style={{ lineHeight: '64px' }}
-    >
-      {items.map(renderMenuItem)}
-    </Menu>
-  </Header>
-)
+class HeaderBar extends Component {
+  state = {
+    popover: false
+  }
+
+  clickLogin = e => {
+    this.setState({ popover: !this.state.popover });
+  }
+
+  renderMenuItem = (menu, index) => (
+    <Menu.Item key={index}>
+      <Link to={`/${menu}`}>{menu}</Link>
+    </Menu.Item>
+  )
+
+  render() {
+    const { items } = this.props;
+    return (
+      <Header>
+        <Row>
+          <Col span={3}>
+            <img src={logo}/>
+          </Col>
+          <Col span={15}>
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['0']}
+              style={{ lineHeight: '64px' }}
+            >
+              {items.map(this.renderMenuItem)}
+            </Menu>
+          </Col>
+          <Col span={3} offset={3}>
+            <Popover
+              content={<LoginForm onClose={this.clickLogin} />}
+              placement="bottomRight"
+              title="Hello"
+              trigger="click"
+              visible={this.state.popover}
+            >
+              <Button ghost onClick={this.clickLogin}>Login</Button>
+            </Popover>
+          </Col>
+        </Row>
+      </Header>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderBar);
