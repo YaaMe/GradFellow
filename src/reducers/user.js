a6e1f78bcadc16b21b4d208e3e2431964454d036
YaaMe <969519545@qq.com>
2018-10-16 02:57:50 +0900

    chore()

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -9,8 +9,10 @@ const initState = {
 export default (state = initState, action) => {
   switch (action.type) {
   case `${ACTION_TYPE.LOGIN}:success`:
-    message.success("Story Created :)");
     return {...state, ...action.data};
+  case `${ACTION_TYPE.SHARE_STORY}:success`:
+    message.success("Story Created :)");
+    return state;
   case ACTION_TYPE.UPDATE_HOME_COUNTRY: return {...state, homeCountry: action.data};
   case ACTION_TYPE.UPDATE_DESIRED_JOB: return {...state, job: action.data};
   case ACTION_TYPE.LOGOUT: return initState;
