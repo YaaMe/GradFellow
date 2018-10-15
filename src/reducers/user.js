e0841b87f9eb61222d9dc182070bec059c0f78a1
YaaMe <969519545@qq.com>
2018-10-16 02:16:56 +0900

    chore(hint)

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,5 +1,5 @@
 import { ACTION_TYPE } from 'actions/user';
-
+import { message } from 'antd';
 const initState = {
   homeCountry: '',
   job: '',
@@ -8,7 +8,9 @@ const initState = {
 
 export default (state = initState, action) => {
   switch (action.type) {
-  case `${ACTION_TYPE.LOGIN}:success`: return {...state, ...action.data};
+  case `${ACTION_TYPE.LOGIN}:success`:
+    message.success("Story Created :)");
+    return {...state, ...action.data};
   case ACTION_TYPE.UPDATE_HOME_COUNTRY: return {...state, homeCountry: action.data};
   case ACTION_TYPE.UPDATE_DESIRED_JOB: return {...state, job: action.data};
   case ACTION_TYPE.LOGOUT: return initState;
